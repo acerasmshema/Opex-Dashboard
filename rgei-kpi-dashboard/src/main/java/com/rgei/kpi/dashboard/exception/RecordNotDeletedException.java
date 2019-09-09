482f1faa14bca5b9dc79b35a8e74652f13582064
aqeeb.pathan <aqeeb.pathan@globallogic.com>
2019-09-09 10:09:20 +0530

    Changes added for exception handling in few controllers

---
+++ rgei-kpi-dashboard/src/main/java/com/rgei/kpi/dashboard/exception/RecordNotDeletedException.java
@@ -0,0 +1,7 @@
+package com.rgei.kpi.dashboard.exception;
+
+public class RecordNotDeletedException extends RuntimeException {
+	public RecordNotDeletedException(String exception) {
+		super(exception);
+	}
+}
